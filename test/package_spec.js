f97a7ee91c5136650b4a26ae992723776bf78da2
Rafael de Oleza <rafeca@gmail.com>
2011-12-04 15:32:52 +0100

    Removed unused test

--- test/package_spec.js
+++ test/package_spec.js
@@ -3,26 +3,11 @@ var fs = require('fs');
 var path = require('path');
 var should = require('should');
 
-describe('package tests', function(){
-  
-  describe('Crappy tests', function(){
-  
-    it('should read correctly the package version', function(){
-    
-      // Get version from package.json
-      var version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')).version;
-      releasetools.version.should.equal(version);
-    });
-  });
-  
-  
-  describe('xusta', function(){
-    it('should read correctly the package version', function(){
-    
-      // Get version from package.json
-      var version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')).version;
-      releasetools.version.should.equal(version);
-    });
+describe('package test', function(){
+  it('should read correctly the package version', function(){    
+    // Get version from package.json
+    var version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')).version;
+    releasetools.version.should.equal(version);
   });
 });
 
