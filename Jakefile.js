62f1078f7af2b9d5061f14037406801cba85f64c
Rafael de Oleza <rafeca@gmail.com>
2011-12-04 19:58:08 +0100

    Updated the annotated documentation in Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,24 @@
 var releaseTools = require('./lib/main');
 var Step = require('step');
 
+// This Jakefile exposes the following tasks using the `releasetools` package:
+//
+//    
+//     jake release:bump      # Bump version in package.json
+//     jake release:build     # Build the new package version
+//     jake release:site      # Create the public site  
+//     jake release:publish   # Publish to GitHub and NPM
+//    
+// * **release:bump** method only increases the version number in the package.json
+//
+// * **release:build** updates the changelog, the contributors files and creates the
+// annotated examples HTML files
+//
+// * **release:site** updates the package public site, with the new version information
+//
+// * **release:publish** commits everything to git, creates the git release tag, pushes to
+// GitHub and publishes the package to NPM
+
 // Customize example files to document via docco
 releaseTools.setOptions({
   examplePaths: [
@@ -8,7 +26,7 @@ releaseTools.setOptions({
   ]
 });
 
-// ## Tests task
+// ## Test task
 // This task executes the package tests by calling the standard `npm test` command
 desc("execute tests");
 task("test",function() {
@@ -35,33 +53,34 @@ task("test",function() {
 
 // Namespace with all the release related tasks
 namespace('release', function() {
+
+  // ## Build task
   desc('Modify the working copy with all the release information');
   task('build', ['test'], function(releaseType) {
-    
     Step(
-      function(){
-        
-        // ### Update Changelog
-        // It updated the `History.md` file with all the commit messages since
-        // last package release (detected by last created tag).
+      
+      // ### Update Changelog
+      // It updated the `History.md` file with all the commit messages since
+      // last package release (detected by last created tag).
+      function() {
         console.log('Updating History.md file...');
         releaseTools.updateChangelog(this);
       },
-      function(err){
+      
+      // ### Update Contributors
+      // It overwrites the AUTHORS file with all the GIT commiters
+      function(err) {
         if (err) throw err;
-        
-        // ### Update Contributors
-        // It overwrites the AUTHORS file with all the GIT commiters 
         console.log('Updating AUTHORS file...');
         releaseTools.updateAuthorsFile(this);
       },
-      function(err){
-        if (err) throw err;
 
-        // ### Create examples
-        // It converts all the specified example files to
-        // HTML files with inline annotated comments, using 
-        // [Docco](http://jashkenas.github.com/docco/)
+      // ### Create examples
+      // It converts all the specified example files to
+      // HTML files with inline annotated comments, using 
+      // [Docco](http://jashkenas.github.com/docco/)
+      function(err) {
+        if (err) throw err;
         console.log('creating examples documentation...');
         releaseTools.createExamples(this);
       },
@@ -72,6 +91,7 @@ namespace('release', function() {
     );
   }, true);
   
+  // ## Create site task
   desc('Create the public site');
   task('site', function(releaseType) {
     // ### Create site
@@ -80,53 +100,54 @@ namespace('release', function() {
     // * The `site/index.html` skeleton file with the contents of the
     //   specified markdown files
     console.log('Creating the public site page...');
-    releaseTools.createSite(function(err){
+    releaseTools.createSite(function(err) {
       if (err) fail();
       else complete();
     });
   }, true);
 
+  // ## Publish task
   desc('Publish to GitHub and NPM the new version');
   task('publish', ['test'] ,function() {
-    
     Step(
+      
+      // Check if the `History.md` is modified
+      // (to ensure that the `release:build` task has been already executed)
       function() {
-        // First of all check if the `History.md` is modified
-        // (to ensure that the `release:build` task has been already executed)
         releaseTools.isWorkingCopyClean('History.md', this);
       },
+      
+      // ### Commit to Git
+      // Makes the commit and creates the tag
       function(err, result) {
         if (err) throw('Error while checking if the git tree is clean: ' + err);
         if (result) throw('You must run jake release:build before publish');
-        
-        // ### Commit to Git
-        // Makes the commit and creates the tag
         console.log('Bumping version and creating git tag...');
         releaseTools.commitToGit(this);
       },
+      
+      // ### Update gh-pages branch
+      // If the branch does not exist, it creates it with the contents of
+      // the `docs/` folder. If it already exists it copies the new contents
+      // of the `docs/` folder into the branch
       function(err) {
         if (err) throw err;
-        
-        // ### Update gh-pages branch
-        // If the branch does not exist, it creates it with the contents of
-        // the `docs/` folder. If it already exists it copies the new contents
-        // of the `docs/` folder into the branch
         console.log('Merging changes into gh-pages branch...');
         releaseTools.updatePagesBranch(this);
       },
+      
+      // ### Push to GitHub
+      // It just does a `git push origin`
       function(err) {
         if (err) throw err;
-        
-        // ### Push to GitHub
-        // It just does a `git push origin`
         console.log('Pushing changes to GitHub...');
         releaseTools.pushToGit(this);
       },
+      
+      // ### Publish to NPM
+      // The same as `npm publish`
       function(err) {
         if (err) throw err;
-      
-        // ### Publish to NPM
-        // The same as `npm publish`
         console.log('Publishing NPM package...');
         releaseTools.npmPublish(this);
       },
